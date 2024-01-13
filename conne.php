060cba880065bb205d315b2dbeb6d91667b72370
nouhaila bijita <152508074+nouhaila1789@users.noreply.github.com>
2024-01-13 10:49:32 +0100

    conne.php

--- conne.php
+++ conne.php
@@ -16,7 +16,7 @@ if(isset($_POST[' adresse email']) and isset($_POST['mot de passe']) )
         {
                 die('Erreur : ' . $e->getMessage());
         }
-    $sql1="select * from utilisateur where email='".$_POST[' adresse email']."'";
+    $sql1="select * from client where email='".$_POST[' adresse email']."'";
     $reponse = $bdd->query($sql1);
     $donnees = $reponse->fetch();
 
@@ -24,10 +24,10 @@ if(isset($_POST[' adresse email']) and isset($_POST['mot de passe']) )
         {   
             $sql2="insert into client(  adresse email, mot de passe) values('".$_POST['email']."','".$_POST['mot de passe'].")";
             $bdd->exec($sql2);
-            echo"<center>Utilisateur ".$_POST['email']." est ajouté avec succés</center>";
+            echo"<center>client ".$_POST['email']." est ajouté avec succés</center>";
         }
         else
-        echo "<center>Utilisateur existe déja !!!</center>";
+        echo "<center>reessayer!!</center>";
     } 
 }
 ?>
